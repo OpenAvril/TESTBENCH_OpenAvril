e58e21a59278c906da02ac2dc1e1ce437db5a8bd
Full Stack Development Template <dragon.scholar@icloud.com>
2026-02-13 21:37:52 +1000

    DEEV_2026_02_13_2137. zero compile errors. run time error - missing .lib files.

--- OpenAvrilConcurrnecy/Program.cs
+++ OpenAvrilConcurrnecy/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//OpenAvril.Library_For_Server_Concurrency.Initialise_Server_Concurrency()
+var pgrmID = OpenAvril.Library_For_Server_Concurrency.Initialise_Server_Concurrency();
